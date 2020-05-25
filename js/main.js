77872c65e246c7817f042e1ca03ca37350c879a6
Gav <g_eyquem@hotmail.com>
2020-05-25 21:12:00 +0100

    js
    changed the js script

--- js/main.js
+++ js/main.js
@@ -1,22 +1,7 @@
 
+
 $('a[data-rel=lightcase:myCollection:slideshow]').lightcase({
   transition: 'scrollHorizontal',
   showSequenceInfo: false,
   showTitle: false
-});
-
-
-$('a[data-rel^=lightcase]').lightcase({
-
-});
-
-$('a').onclick(function(){
-
-
-});
-
-
-$('.image').scroll(function(){
-
-
 });
